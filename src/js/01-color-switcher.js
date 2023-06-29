48b2439ad55b2d84707bb85435445f0a3ede899e
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-29 07:23:55 -0400

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,96 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyStyles : document.querySelector('body')
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+const COLORCHANGE_DELAY = 1000;
+let intervalId = null;
+refs.stopBtn.disabled = true;
+
+
+
+function onStartBtnClick(){
+  intervalId = setInterval(() => {
+    refs.bodyStyles.style.backgroundColor = getRandomHexColor();
+  },COLORCHANGE_DELAY);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  refs.stopBtn.style.cssText = `
+background-color: #f44336;
+color: white;
+padding: 20px 40px;
+border: none;
+cursor: pointer;
+border-radius: 40px;
+font-size: 20px;`;
+
+refs.startBtn.style.cssText = `
+background-color: grey;
+color: white;
+padding: 20px 40px;
+border: none;
+cursor: pointer;
+border-radius: 40px;
+font-size: 20px;`;
+
+};
+
+function onStopBtnClick(){
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  refs.startBtn.style.cssText = `
+background-color: #4CAF50;
+color: white;
+padding: 20px 40px;
+border: none;
+cursor: pointer;
+border-radius: 40px;
+font-size: 20px;`;
+
+refs.stopBtn.style.cssText = `
+background-color: grey;
+color: white;
+padding: 20px 40px;
+border: none;
+cursor: pointer;
+border-radius: 40px;
+font-size: 20px;`;
+};
+
+refs.bodyStyles.style.cssText = `
+display: flex; 
+gap: 20px; 
+align-items: center;
+justify-content: center;
+padding-top: 200px;`;
+
+
+refs.startBtn.style.cssText = `
+background-color: #4CAF50;
+color: white;
+padding: 20px 40px;
+border: none;
+cursor: pointer;
+border-radius: 40px;
+font-size: 20px;`;
+
+refs.stopBtn.style.cssText = `
+background-color: #f44336;
+color: white;
+padding: 20px 40px;
+border: none;
+cursor: pointer;
+border-radius: 40px;
+font-size: 20px;`;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
